24304f2bd9fe3e71e50afa423837968b98ee20c8
waygon32 <truonthien2310@gmail.com>
2021-02-04 14:18:05 +0700

    ,

---
+++ suDungToanTu/src/sudungtoantu.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sudungtoantu {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width = sc.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = sc.nextFloat();
+
+        float area = width * height;
+        System.out.println("Area is: " + area);
+    }
+}
---
+++ time/src/displayTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class displayTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("date now "+ now);
+    }
+}
